1cb9a21a53d844336a050bd674aa43f7e206d37b
karam karim <BlackGhost3@users.noreply.github.com>
2017-03-22 22:35:44 +0300

    Update api.js
    removed something that i should've removed before I uploaded the project

--- app/resources/js/api.js
+++ app/resources/js/api.js
@@ -4,7 +4,7 @@ let api = {};
 api.bf = function (gt, plat, g) {
     return new Promise(function(resolve, reject) {
         const BattlefieldStats = require('battlefield-stats');
-        const API_KEY = 'fb72d43d-1bc9-4e3e-85e3-256daa99f436' // from https://battlefieldtracker.com/site-api
+        const API_KEY = 'YOUR API KEY'; // get it from https://battlefieldtracker.com/site-api
         const bf = new BattlefieldStats(API_KEY);
 
         // All params mirror params listed at http://docs.trnbattlefield.apiary.io/#
